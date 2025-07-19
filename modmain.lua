0db52349390ab62a6c66988c521444dc05cf6d09
Luкaട <onlyt.coding@gmail.com>
2025-07-19 21:38:11 +0200

    Placed Game Options override in a separate file for clarity.

--- modmain.lua
+++ modmain.lua
@@ -9,17 +9,20 @@ PrefabFiles = {
     "mc_item_shadows"
 }
 
+-- [[ Mod environment ]]
 modimport("scripts/MYIenv")
 
-local function TagsCheck(inst)
-    return not inst:HasTag("flying") and
-           not inst:HasTag("heavy") and
-           not inst:HasTag("structure") and
-           not inst:HasTag("furnituredecor")
+if _G.MYI.DEV then
+    _G.MYI.inspect = require("inspect")
 end
+--
 
+-- Custom settings because configuration_options are annoying to use
+modimport("scripts/MYImodsettings")
+
+-- Misc. changes
 AddPrefabPostInitAny(function(inst)
-    if not _G.TheNet:IsDedicated() and inst.AnimState and TagsCheck(inst) then
+    if not _G.TheNet:IsDedicated() and inst.AnimState then
         inst:DoTaskInTime(0, function() -- Wait 1 frame for the replica components to get created on the client
             if inst.replica.inventoryitem and not inst.replica.combat then
                 inst:AddComponent("MYImanager")
@@ -35,62 +38,3 @@ AddPlayerPostInit(function(inst)
         inst.mc_items = {  } -- Holds information about existing MC items to generate shadows under them
     end
 end)
-
-local MYISettingsTab = require("widgets/MYIsettingstab")
-local OptionsScreen = require("screens/redux/optionsscreen")
-local old_OptionsScreen_BuildMenu = OptionsScreen._BuildMenu
-OptionsScreen._BuildMenu = function(self, subscreener, ...)
-    subscreener.sub_screens["MYI"] = self.panel_root:AddChild(MYISettingsTab(self))
-    local menu = old_OptionsScreen_BuildMenu(self, subscreener, ...)
-
-	local myi_button = subscreener:MenuButton(_G.MYI.SETTINGS.NAME, "MYI", _G.MYI.SETTINGS.TOOLTIP, self.tooltip)
-    menu:AddCustomItem(myi_button)
-    local pos = _G.Vector3(0, 0, 0)
-    pos.y = pos.y + menu.offset * (#menu.items - 1)
-    myi_button:SetPosition(pos)
-    
-    return menu
-end
-
-local function EnabledOptionsIndex(enabled)
-    return enabled and 2 or 1
-end
-
-if _G.Profile:GetValue(_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR) == nil then
-    _G.Profile:SetValue(_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR, _G.MYI.SETTINGS.OPTIONS.WORLD_Y.DEFAULT)
-end
-
-if _G.Profile:GetValue(_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR) == nil then
-    _G.Profile:SetValue(_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR, _G.MYI.SETTINGS.OPTIONS.SHADOWS.DEFAULT)
-end
-
-local old_OptionsScreen_DoInit = OptionsScreen.DoInit
-OptionsScreen.DoInit = function(self, ...)
-    self.options[_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR] = _G.Profile:GetValue(_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR)
-    self.working[_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR] = _G.Profile:GetValue(_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR)
-
-    self.options[_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR] = _G.Profile:GetValue(_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR)
-    self.working[_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR] = _G.Profile:GetValue(_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR)
-
-    old_OptionsScreen_DoInit(self, ...)
-end
-
-local old_OptionsScreen_Apply = OptionsScreen.Apply
-OptionsScreen.Apply = function(self, ...)
-    _G.Profile:SetValue(_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR, self.working[_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR])
-    _G.Profile:SetValue(_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR, self.working[_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR])
-
-    if _G.ThePlayer then -- Player exists == we're changing settings during playtime
-        _G.ThePlayer.mc_items = {  } -- Reset the mc item tracker
-    end
-
-    old_OptionsScreen_Apply(self, ...)
-end
-
-local old_OptionsScreen_InitializeSpinners = OptionsScreen.InitializeSpinners
-OptionsScreen.InitializeSpinners = function(self, ...)
-    self.subscreener.sub_screens["MYI"].worldYSpinner:SetSelectedIndex(EnabledOptionsIndex(self.working[_G.MYI.SETTINGS.OPTIONS.WORLD_Y.OPTIONS_STR]))
-    self.subscreener.sub_screens["MYI"].shadowsSpinner:SetSelectedIndex(EnabledOptionsIndex(self.working[_G.MYI.SETTINGS.OPTIONS.SHADOWS.OPTIONS_STR]))
-
-    old_OptionsScreen_InitializeSpinners(self, ...)
-end
---
+++ scripts/MYImodsettings.lua
@@ -0,0 +1,72 @@
+-- [[ Define default values ]]
+local loaded_settings = {  }
+_G.MYI.modgetpersistentdata(_G.MYI.MOD_SETTINGS.FILENAME, function(_, data)
+    if data == "" then -- Run only when the settings file is missing
+        for _, setting in pairs(_G.MYI.MOD_SETTINGS.SETTINGS) do
+            if loaded_settings[setting.ID] == nil then
+                loaded_settings[setting.ID] = setting.DEFAULT
+            end
+        end
+
+        _G.MYI.modsetpersistentdata(_G.MYI.MOD_SETTINGS.FILENAME, _G.json.encode(loaded_settings))
+    else -- Otherwise load existing settings, even if some might be missing
+        loaded_settings = _G.json.decode(data)
+    end
+end)
+
+-- [[ Add mod settings to the Game Options screen ]]
+local MYISettingsTab = require("widgets/MYIsettingstab")
+local OptionsScreen = require("screens/redux/optionsscreen")
+local old_OptionsScreen_BuildMenu = OptionsScreen._BuildMenu
+OptionsScreen._BuildMenu = function(self, subscreener, ...)
+    subscreener.sub_screens[_G.MYI.MOD_CODE] = self.panel_root:AddChild(MYISettingsTab(self, loaded_settings))
+    local menu = old_OptionsScreen_BuildMenu(self, subscreener, ...)
+
+	local myi_button = subscreener:MenuButton(_G.MYI.MOD_SETTINGS.TAB_NAME, _G.MYI.MOD_CODE, _G.MYI.MOD_SETTINGS.TOOLTIP, self.tooltip)
+    menu:AddCustomItem(myi_button)
+    local pos = _G.Vector3(0, 0, 0)
+    pos.y = pos.y + menu.offset * (#menu.items - 1)
+    myi_button:SetPosition(pos)
+    
+    return menu
+end
+
+local old_OptionsScreen_DoInit = OptionsScreen.DoInit
+OptionsScreen.DoInit = function(self, ...)
+    for id, setting in pairs(loaded_settings) do
+        self.options[id] = setting
+        self.working[id] = setting
+    end
+
+    old_OptionsScreen_DoInit(self, ...)
+end
+
+local old_OptionsScreen_Apply = OptionsScreen.Apply
+OptionsScreen.Apply = function(self, ...)
+    for _, setting in pairs(_G.MYI.MOD_SETTINGS.SETTINGS) do
+        loaded_settings[setting.ID] = self.working[setting.ID]
+    end
+
+    _G.MYI.modsetpersistentdata(_G.MYI.MOD_SETTINGS.FILENAME, loaded_settings, function()
+        if _G.ThePlayer then -- Player exists == we're changing settings during playtime
+            _G.ThePlayer.mc_items = {  } -- Reset the mc item tracker
+        end
+    end)
+
+    old_OptionsScreen_Apply(self, ...)
+end
+
+local function EnabledOptionsIndex(enabled)
+    return enabled and 2 or 1
+end
+
+local old_OptionsScreen_InitializeSpinners = OptionsScreen.InitializeSpinners
+OptionsScreen.InitializeSpinners = function(self, ...)
+    for _, w in pairs(self.subscreener.sub_screens[_G.MYI.MOD_CODE].left_column) do
+        if w.SetSelectedIndex then
+            w:SetSelectedIndex(EnabledOptionsIndex(self.working[w.setting_id]))
+        end
+    end
+
+    old_OptionsScreen_InitializeSpinners(self, ...)
+end
